3c53ac07759accd25decf7d2092e5995d8a9247c
HUGH228 <105647692+HUGH228@users.noreply.github.com>
2023-04-09 12:00:00 +0000

    update

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 500;
-canvas.height = 500;
-
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2.5;
+canvas.width = 700;
+canvas.height = 700;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = "#000000";
+ctx.lineWidth = 2.5; /* 라인 굵기 */
 
 let painting = false;
 
-function startPainting() {
-    painting=true;
+function stopPainting() {
+    painting = false;
 }
-function stopPainting(event) {
-    painting=false;
+
+function startPainting() {
+    painting = true;
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL("image/png");
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting) {
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
-    }
-    else {
+    } else{
         ctx.lineTo(x, y);
         ctx.stroke();
     }
 }
+
+function onMouseDown(event) {
+    painting = true;
+}
+
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -35,3 +46,11 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+if (saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
+const link = document.createElement("a");
+link.download = "PaintJS[EXPORT]";
+link.click();
